89342641a4f8ba0c66874dada767a8b118446c90
Bhavesh <bhaveshmotiramani97@gmail.com>
2020-02-20 22:58:33 +0530

    side nav bar fixed and social media links fixed for smaller screens

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
+var countMenuToggleClicks = 0;
+
 $('.menu-toggle').click(function(){
+   
     $('.site-nav').toggleClass('site-nav--open',500); //toggleClass after 500ms
 
+    //Adding/Removing blur
+    $('section').toggleClass('content');
+    $('footer').toggleClass('content');
     //Adding X
     $(this).toggleClass('open');
   
+    //when clicked on list item
+    $('.site-nav li').click(function(){
+
+         
+            $('.site-nav').removeClass('site-nav--open',500);
+            //remove the X
+            $('.menu-toggle').removeClass('open');
+            //Removing blur
+            $('section').removeClass('content');
+            $('footer').removeClass('content');
+    });
+
+   
 });
 
 
